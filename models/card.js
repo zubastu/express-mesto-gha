1fb9ea836a196ee34a557ede00b03452a72d6a5c
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-05 11:23:26 +0300

    Закончил создание моделей пользователя и карточки

--- models/card.js
+++ models/card.js
@@ -3,17 +3,26 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   link: {
     type: String,
+    required: true,
   },
   owner: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
   },
   likes: {
-    type: Array,
+    type: mongoose.Schema.Types.ObjectId,
+    default: [],
   },
   createdAt: {
     type: Date,
+    default: Date.now(),
   },
 });
+
+module.exports = mongoose.model('user', cardSchema);
