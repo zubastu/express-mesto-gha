e81498b095a2f6afeea2296eb75bee2aa888782e
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-09 18:55:16 +0300

    Добавил обработку ошибки несуществующего айди

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,8 +32,8 @@ module.exports.deleteCard = (req, res) => {
       return res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'UndefinedId') return res.status(404).send({ message: err.message });
-      if (err.name === 'InvalidId') return res.status(400).send({ message: err.message });
+      if (err.name === 'UndefinedId') return res.status(400).send({ message: err.message });
+      if (err.name === 'InvalidId') return res.status(404).send({ message: err.message });
       return res.status(500).send({ message: err.message });
     });
 };
