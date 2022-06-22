da1a2f04e3f4752c56dfdd129994165b2bc4501d
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-22 21:10:04 +0300

    Добавил в обработку ошибок функционал: Создал кастомные ошибки и их обработку с ответом

--- utils/errors.js
+++ utils/errors.js
@@ -5,12 +5,57 @@ const NOT_FOUND = 404;
 const REGISTRATION_WITH_USED_EMAIL = 409;
 const DEFAULT_ERROR_CODE = 500;
 
+class badRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'BadRequest';
+  }
+}
+
+class wrongPassword extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'WrongPassword';
+  }
+}
+
+class wrongOwner extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'WrongOwner';
+  }
+}
+
+class notFound extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'NotFound';
+  }
+}
+
+class usedEmail extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'UsedEmail';
+  }
+}
+
 module.exports.errorProcessing = (err, res) => {
   switch (err.name) {
     case 'ValidationError':
       return res.status(BAD_REQ).send({ message: `Ошибка передачи данных, все поля должны быть заполнены корректно: ${err.message}` });
+    case 'BadRequest':
+      return res.status(BAD_REQ).send({ message: `Не найдено по входным данным: ${err.message}` });
     case 'CastError':
       return res.status(BAD_REQ).send({ message: `Не найдено по входным данным: ${err.message}` });
+    case 'WrongPassword':
+      return res.status(WRONG_PASS_OR_EMAIL).send({ message: `Не найдено по входным данным: ${err.message}` });
+    case 'WrongOwner':
+      return res.status(WRONG_OWNER).send({ message: `Не найдено по входным данным: ${err.message}` });
+    case 'NotFound':
+      return res.status(NOT_FOUND).send({ message: `Не найдено по входным данным: ${err.message}` });
+    case 'UsedEmail':
+      return res.status(REGISTRATION_WITH_USED_EMAIL).send({ message: `Ошибка: ${err.message}` });
     default:
       return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
   }
@@ -28,4 +73,8 @@ module.exports.checkUser = (data, res) => {
     return res.status(WRONG_PASS_OR_EMAIL).send({ message: 'Не правильная почта или пароль' });
   }
   return res.send({ data });
+};
+
+module.exports = {
+  badRequest, wrongPassword, wrongOwner, notFound, usedEmail,
 };
