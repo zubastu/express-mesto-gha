30bb7d1d83e330ac7793cfd446064b1c2fcf690c
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-23 11:42:37 +0300

    Добавил в обработку ошибок функционал: Joi

--- models/user.js
+++ models/user.js
@@ -7,10 +7,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     default: 'Жак-ив Кусто',
+    minlength: 2,
+    maxlength: 30,
   },
   about: {
     type: String,
     default: 'Исследователь',
+    minlength: 2,
+    maxlength: 30,
   },
   avatar: {
     type: String,
