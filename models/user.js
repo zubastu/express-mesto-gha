725ca5cd7820518abeb9d92ab5f2987d23688837
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-23 12:06:39 +0300

    Добавил в обработку ошибок функционал: Joiа

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (value) => {
-        if (validator.isURL(value)) {
+        if (!validator.isURL(value)) {
           throw new Error('Поле avatar должно быть корректным');
         }
       },
