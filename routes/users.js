41a2f289c4f309d3354a15ae5cbb57b7fe2a5543
zubastu <79757684+zubastu@users.noreply.github.com>
2022-06-23 11:24:52 +0300

    Добавил в обработку ошибок функционал: Joi

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,31 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getUsers, getUser, patchUserInfo, patchAvatar, getUserSelf,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.patch('/me', patchUserInfo);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), patchUserInfo);
+
 router.get('/me', getUserSelf);
-router.patch('/me/avatar', patchAvatar);
-router.get('/:id', getUser);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+  }),
+}), patchAvatar);
+
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+}), getUser);
 
 module.exports = router;
